2b8d595dc09e0c6368b222a15e7076751eca82b8
recentlymaterialized <118731633+recentlymaterialized@users.noreply.github.com>
2022-12-25 16:29:36 +0100

    allow floating point values for random fill %

--- cpp/Console viewer.cpp
+++ cpp/Console viewer.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstdint>
+#include <charconv>
 #include <random>
 #include <chrono>
 #include "CellGrid.h"
@@ -12,15 +13,13 @@ int main() {
     std::random_device rd;
     std::seed_seq sq{
         rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd(),
-        static_cast<std::uint32_t>(std::chrono::steady_clock::now().time_since_epoch().count())
+        static_cast<unsigned int>(std::chrono::steady_clock::now().time_since_epoch().count())
     };
     std::mt19937 prng{sq};
-    std::uniform_int_distribution bin{0, 1};
-    std::uniform_int_distribution cent{0, 99};
     constexpr auto cinlim{ std::numeric_limits<std::streamsize>::max() };
 
     char rule[21];
-    std::int_fast16_t width{}, height{}, fill{};
+    std::int_fast16_t width{}, height{};
     std::int_fast32_t simlen{-1};
     cout << "Cellular automata simulation\n";
     do {
@@ -34,7 +33,14 @@ int main() {
         cin.ignore(cinlim, '\n');
     } while (height < 1 || height > 1855);
 
-    CellGrid grid(width, height);
+    /** // testing various parts of CellGrid; this code reveals a bunch of issues that I have no idea how to fix
+    CellGrid pgrid(width, height, 3076l);
+    CellGrid grid(pgrid, 13, 30483, 1234, CellGrid::torus);
+    grid.rule = 3076l;
+    grid.resize(width, height);
+    /*/
+    CellGrid grid(width, height, "", CellGrid::torus);
+    //*/
     bool retry{};
     do {
         cout << (retry ? "Invalid rule\nRule: " : "Rule: ");
@@ -42,28 +48,60 @@ int main() {
         cin.ignore(cinlim, '\n');
         retry = true;
     } while (!(grid.rule = rule));
+
+    char inStr[16];
     do {
-        cout << "Random fill percentage (%): "; // currently uses int; i will switch to floats soon
-        if (!(cin >> fill)) cin.clear();
+        cout << "Random fill percentage (%): ";
+        if (cin >> inStr) {
+            if (*inStr == '.' || inStr[1] == '.' || inStr[2] == '.') {
+                long double fillD{};
+                std::from_chars(inStr, inStr + 16, fillD);
+                fillD /= 100.l;
+                if (fillD && fillD >=0 && fillD <= 1.l) { // >=0 makes an angry face
+                    retry = false;
+                    unsigned int threshold{ static_cast<unsigned int>(fillD * ((long double)UINT_MAX + 1.l)) };
+                    // is UINT_MAX in the official C++ specification?
+                    if (threshold) for (int x, y{ height / 8 }, xlim{ 7 * width / 8 }; y != 7 * height / 8; ++y)
+                        for (x = width / 8; x != xlim; ++x) grid(x, y, prng() < threshold);
+                    else if (fillD < 1) retry = true;
+                    else for (int x, y{ height / 8 }; y != 7 * height / 8; ++y)
+                        for (x = width / 8; x != 7 * width / 8; ++x) grid(x, y, true);
+                }
+            }
+            else {
+                int fillI{};
+                std::from_chars(inStr, inStr + 8, fillI);
+                if (fillI && fillI >=0 && fillI < 101) {
+                    if (fillI == 100) for (int x, y{ height / 8 }; y != 7 * height / 8; ++y)
+                        for (x = width / 8; x != 7 * width / 8; ++x) grid(x, y, true);
+                    else if (fillI == 50) {
+                        std::uniform_int_distribution bin{0, 1};
+                        for (int x, y{ height / 8 }; y != 7 * height / 8; ++y)
+                            for (x = width / 8; x != 7 * width / 8; ++x) grid(x, y, bin(prng));
+                    }
+                    else {
+                        std::uniform_int_distribution cent{0, 99};
+                        for (int x, y{ height / 8 }; y != 7 * height / 8; ++y)
+                            for (x = width / 8; x != 7 * width / 8; ++x) grid(x, y, cent(prng) < fillI);
+                    }
+                    retry = false;
+                }
+            }
+        }
+        else cin.clear();
         cin.ignore(cinlim, '\n');
-    } while (fill < 1 || fill > 100);
+    } while (retry);
+
     do {
         cout << "Simulation length: ";
         if (!(cin >> simlen)) cin.clear();
         cin.ignore(cinlim, '\n');
     } while (simlen < 0 || simlen > 65535l);
 
-    if (fill == 50) for (int x, y{ height / 8 }; y != 7 * height / 8; ++y)
-        for (x = width / 8; x != 7 * width / 8; ++x) grid(x, y, bin(prng));
-    else for (int x, y{ height / 8 }; y != 7 * height / 8; ++y)
-        for (x = width / 8; x != 7 * width / 8; ++x) grid(x, y, cent(prng) < fill);
-    cout << "\nStart grid: ";
-    grid.print(true);
-
-    grid += simlen;
-    cout << "\n\nFinal grid - ";
-    grid.print(true);
-
+    cout << "\nStart grid: \n" << grid;
+    cout << "\n\nFinal grid - Generation " << simlen << ":\n"
+         << (grid += simlen);
+    
     cout << "\n\nProgram completed\n";
     char temp;
     cin >> temp;
